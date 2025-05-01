da626439995c103839f8cf5541d8c3792b05b317
Sahil Shrestha <137997098+lihasahil@users.noreply.github.com>
2025-05-01 13:38:48 +0545

    Update ExperienceSection.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -96,7 +96,7 @@ const ExperienceSection = () => {
       <div className="w-full h-full md:px-20 px-5">
         <TitleHeader
           title="Professional Work Experience"
-          sub="💼 My Career Overview"
+          sub="💼 My Career Overview(Imaginary-Showing Skills)"
         />
         <div className="mt-32 relative">
           <div className="relative z-50 xl:space-y-32 space-y-10">
